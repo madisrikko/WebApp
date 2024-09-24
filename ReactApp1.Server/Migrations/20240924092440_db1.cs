824005bd3ce0b0a5712d17f81257c5a7d6ce1b55
Madis Rikko <92031670+madisrikko@users.noreply.github.com>
2024-09-24 12:26:25 +0300

    fix (#5)
    Co-authored-by: NETGROUPDIGITAL\madis.rikko <madis.rikko@netgroup.com>

--- ReactApp1.Server/Migrations/20240924092440_db1.cs
+++ ReactApp1.Server/Migrations/20240924092440_db1.cs
@@ -30,6 +30,7 @@ namespace ReactApp1.Server.Migrations
                 columns: table => new
                 {
                     Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    ProfilePictureUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
                     UserName = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
                     NormalizedUserName = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
                     Email = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
